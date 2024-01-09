72296a7d4b01d46971c4dddda1b21fa65456fcbe
AlwaysHungry <suico.dave@gmail.com>
2024-01-09 19:43:55 +0800

    fix typo

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -27,7 +27,7 @@ class AppServiceProvider extends ServiceProvider
         Schema::defaultStringLength(191);
 
         if (config('app.env') === 'production' || config('app.env') === 'staging') {
-            URL::forceScheme('https');
+            \URL::forceScheme('https');
         }
     }
 }
